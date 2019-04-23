84f6fd692c4117137edd2fad8940cddc2095e1e4
eduard-sd <pr-spektr@yandex.ru>
2019-04-24 01:05:40 +0300

    In the end

--- HTML/Rospartner/js/main.js
+++ HTML/Rospartner/js/main.js
@@ -1,59 +1,151 @@
-const formList = document.querySelectorAll('.form input, .form textarea'), //elements
+const formList = document.querySelectorAll('.form__input-underline, .form textarea'), //elements
 	  checkbox = document.querySelector('#checkbox'),
 	  workGroupPublic = document.querySelector('.form__input-work-group-public'),
 	  workGroupTextarea = document.querySelectorAll('.form__input-textarea'),
 	  labelsTextarea = document.querySelectorAll('.form__input-labels-textarea'),
-	  alert = document.querySelector('.form__attention'),
-	  inputFields = document.querySelector('.form__input-fields');
+	  alertDiv = document.querySelector('.form__attention'),
+	  inputFields = document.querySelector('.form__input-fields'),
+	  saveButton = document.querySelector('.form__button-saveform'),
+	  resetButton = document.querySelector('.form__button-reset'),
+	  submitButton = document.querySelector('.form__button-submit'),
+	  buttonsContainer = document.querySelectorAll('.form__input-fields-row3');
+
 
 
 function checkRefresh() {
+	let alertVisabilty = true;
 	Object.keys(sessionStorage).forEach(function(key){
 		let curentInput = document.querySelector("#"+`${key}`);
 		curentInput.value = sessionStorage.getItem(key);  // Восстанавливаем содержимое текстового поля
 	});
 
+	const checkFields = (element) => {
+		console.log(element, element.previousElementSibling, element.previousElementSibling.id);
+		let parentDiv = element.parentElement,
+			label = element.previousElementSibling.id,
+			labelSelector = document.querySelector('#'+label);
+
+		if (!element.checkValidity()) {
+			parentDiv.classList.add('form__input-fields-row1-invalid');
+			labelSelector.style.color = '#F64C4C';
+
+		}
+		if (element.checkValidity()) {
+			parentDiv.classList.remove('form__input-fields-row1-invalid');
+			labelSelector.style.color = 'rgba(0, 0, 0, 0.5)';
+		}
+		if (element.value.length === 0) {
+			parentDiv.classList.remove('form__input-fields-row1-invalid');
+			labelSelector.style.color = '#000000';
+		}
+	};
+
 	formList.forEach(function(element){
 		element.addEventListener('change', function() {
 			sessionStorage.setItem(`${element.id}`, element.value);
+			checkFields(element);
 
-			let parentDiv = element.parentElement,
-				label = element.previousElementSibling.id,
-				labelSelector = document.querySelector('#'+label);
-
-			if (!element.checkValidity()) {
-				parentDiv.classList.add('form__input-fields-row1-invalid');
-				labelSelector.style.color = '#F64C4C';
-
+			if (sessionStorage.length > 0) {
+				saveButton.removeAttribute("disabled");
+				submitButton.removeAttribute("disabled");
 			}
-			if (element.checkValidity() || element.value.length===0) {
-				parentDiv.classList.remove('form__input-fields-row1-invalid');
-				labelSelector.style.color = 'rgba(0, 0, 0, 0.5)';
+
+			if (element.value.length === 0) {
+				sessionStorage.removeItem(element.id);
+				saveButton.setAttribute("disabled",true);
+				submitButton.setAttribute("disabled",true);
 			}
 		});
+
+		element.addEventListener('click', function() {
+
+		});
+
+		checkFields(element);
 	});
 
-	if (sessionStorage.length > 0) { // animation for alert
-		alert.style.display = 'block';
-		let alertPos = -1000,
-			animate = setInterval(frame, 10),
-			opacityAlert = 0,
-			inputFieldsPos = 20;
 
-		function frame() {
-			if (alertPos >= -100) {
-				clearInterval(animate);
-			} else {
-				alertPos += 5;
-				opacityAlert += .005;
-				inputFieldsPos += 0.5;
+	const alertAnimation = (event) => {
+		if (event) {
+			alertDiv.style.display = 'block';
+			let alertPos = -1000,
+				animate = setInterval(moveRight, 10),
+				opacityAlert = 0,
+				inputFieldsPos = 20;
 
-				alert.style.left = alertPos + "px";
-				alert.style.opacity = opacityAlert;
-				inputFields.style.marginTop = inputFieldsPos+"px";
+			function moveRight() { // animation for alert
+				if (alertPos >= -100) {
+					clearInterval(animate);
+				} else {
+					alertPos += 5;
+					opacityAlert += .005;
+					inputFieldsPos += 0.5;
+
+					alertDiv.style.left = alertPos + "px";
+					alertDiv.style.opacity = opacityAlert;
+					inputFields.style.marginTop = inputFieldsPos+"px";
+				}
 			}
+			alertVisabilty = false;
+		}else if (!event) {
+			alertDiv.style.display = 'block';
+			let alertPos = -100,
+				animate = setInterval(moveLeft, 10),
+				opacityAlert = 1,
+				inputFieldsPos = 90;
+
+			function moveLeft() { // animation for alertDiv
+				if (alertPos <= -1000) {
+					clearInterval(animate);
+				} else {
+					alertPos -= 5;
+					opacityAlert -= .005;
+					inputFieldsPos -= 0.5;
+
+					alertDiv.style.left = alertPos + "px";
+					alertDiv.style.opacity = opacityAlert;
+					inputFields.style.marginTop = inputFieldsPos+"px";
+				}
+
+			}
+			alertVisabilty = true;
 		}
+	};
+
+	if (sessionStorage.length > 0) {
+		alertAnimation(alertVisabilty);
+		saveButton.removeAttribute("disabled");
+		submitButton.removeAttribute("disabled");
 	}
+
+	submitButton.onclick = function (event) {
+		event.preventDefault();
+
+		alertAnimation(false);
+		formList.forEach(function(element) {
+			checkFields(element);
+			if (element.checkValidity() && element.value.length >= 0){
+				//очистить поля
+			}
+		});
+
+	};
+
+	resetButton.onclick = function () {
+		console.log(1);
+		saveButton.setAttribute("disabled",true);
+		submitButton.setAttribute("disabled",true);
+
+		Object.keys(sessionStorage).forEach(function(key){
+			sessionStorage.removeItem(key);
+		});
+		formList.forEach(function(element) {
+			element.value = "";
+			checkFields(element);
+		});
+		// openBox();
+
+	};
 }
 
 
@@ -75,82 +167,78 @@ let box = true;
 checkbox.addEventListener('change', () => {openBox()});
 
 
+const tempWithTextarea = document.querySelectorAll('.form__input-fields-row2');
 
+tempWithTextarea.forEach((el) => {
+	const childrens = el.children;
+	const label = childrens[0];
+	const textarea = childrens[1];
 
-let textareaFocused = false;
-let isMouseInTextarea = false;
-let isMouseInLabel = false;
+	textarea.onmouseover = (e) => {
+		textarea.classList.add('hovered');
 
-workGroupTextarea.forEach(function (element) { // event for textarea
-	const labels = element.previousElementSibling;
+		if (textarea.classList.contains('focused')) return;
+		label.style.background = '#f1f5f9';
 
-	element.onmouseover = function(event) {
-		if (!textareaFocused) {
-			labels.style.background = '#F1F5F9';
-		}
-		isMouseInTextarea = true;
 	};
 
-	element.onmouseout = function(event) {
-		if (!textareaFocused) {
-			labels.style.background = '';
-		}
-		isMouseInTextarea = false;
+	textarea.onmouseout = (e) => {
+		textarea.classList.remove('hovered');
+
+		if (textarea.classList.contains('focused')) return;
+		label.style.background = '';
+
 	};
 
-	element.onfocus = function(event) {
-		labels.style.cssText = 'border-top: 2px solid rgba(47, 128, 237, 0.75);' +
-			'					background-color: #FFFFFF;';
+	textarea.onfocus = (e) => {
+		label.style.cssText = 'border-top: 2px solid rgba(47, 128, 237, 0.75);' +
+			'					background-color: #FFFFFF; color: #2F80ED;' ;
 
-		element.style.cssText = 'background-image: linear-gradient(#FFFFFF, #FFFFFF 18px, #000 28px, #000 28px, #FFFFFF 18px);\n' +
-			'    background-size: 100% 19px;\n' +
-			'    background-color: #FFFFFF;' +
-			'	 background-position: 50% 100%;' +
-			'	 border: 2px solid rgba(47, 128, 237, 0.75)';
+		textarea.style.cssText = 'background-image: linear-gradient(#FFFFFF, #FFFFFF 18px, #000 28px, #000 28px, #FFFFFF 18px);\n' +
+								'    background-size: 100% 19px;\n' +
+								'    background-color: #FFFFFF;' +
+								'	 background-position: 50% 100%;' +
+								'	 border: 2px solid rgba(47, 128, 237, 0.75)';
 
-		textareaFocused = true;
+		textarea.classList.add('focused');
 	};
 
-	element.onblur = function(event) {
-		if (isMouseInTextarea) {
-			labels.style.cssText = `borderTop: ""; background-color: #f1f5f9;`
+	textarea.onblur = (e) => {
+		console.log(textarea.classList);
+		if(textarea.classList.contains('hovered')) {
+			label.style.cssText = `borderTop: ""; background-color: #f1f5f9; color: #000000;`
 		} else {
-			labels.style.cssText = `borderTop: "";
-									background-color: "";`;
+			console.log('here');
+			label.style.cssText = `borderTop: ""; background-color: "";`;
 		}
 
-		if (isMouseInLabel) {
-			labels.style.cssText = `borderTop: ""; background-color: #f1f5f9;`
-		}
-
-		element.style.cssText = 'background-image: "";\n' +
+		textarea.style.cssText = 'background-image: "";\n' +
 			'    background-size: "";\n' +
 			'    background-color: "";' +
 			'	 background-position: "";' +
 			'	 border: ""';
-		textareaFocused = false;
+
+
+		textarea.classList.remove('focused');
+
+		if (textarea.value.length) {
+			label.style.cssText = `borderTop: ""; background-color: ""; color: rgba(0, 0, 0, 0.5);`;
+		}
+	};
+
+	label.onmouseover = (e) => {
+		if (textarea.classList.contains('focused')) {
+			label.style.background = '#FFFFFF';
+			return;
+		}
+		label.style.background = '#f1f5f9';
+	};
+
+	label.onmouseout = (e) => {
+		if (textarea.classList.contains('focused')) {
+			label.style.background = '#FFFFFF';
+			return;
+		}
+		label.style.background = '';
 	}
-});
-
-
-labelsTextarea.forEach(function (element) { //pseudo event for label of textarea
-	const textarea = element.nextElementSibling;
-
-	element.onmouseover = function(event) {
-		if (textareaFocused) {
-			element.style.background = '#FFFFFF';
-		} else {
-			element.style.background = '#F1F5F9';
-		}
-		isMouseInLabel = true;
-	};
-
-	element.onmouseout = function(event) {
-		if (textareaFocused) {
-			element.style.background = '#FFFFFF';
-		} else {
-			element.style.background = '';
-		}
-		isMouseInLabel = false;
-	};
 });
