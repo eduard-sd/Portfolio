9ef27fa0b0099930e6011ec9c54087b5aed41c7f
eduard-sd <pr-spektr@yandex.ru>
2019-05-27 16:09:09 +0300

    Tatmedia random refactor

--- HTML/Tatmedia/src/js/main.js
+++ HTML/Tatmedia/src/js/main.js
@@ -1,8 +1,23 @@
-const newBlocks = document.querySelector(".hot-news__container");
 
+const blocks = document.querySelector(".hot-news__container");
 
-for(let i = 0; i < newBlocks.children.length; i++){
-    let target = Math.floor(Math.random() * newBlocks.children.length -1) + 1;
-    newBlocks.children[i].style.order = `${target}`;
+const renderBlocks = (index) => {
+    const item = `
+            <div class="col-row-1">A${index}</div>
+            <div class="col-row-2">B${index}</div>
+            <div class="col-row-3">C${index}</div> 
+    `;
+    blocks.insertAdjacentHTML("beforeend", item);
+};
+
+for (let i = 1; i <= 10; i++) {
+    renderBlocks(i)
 }
+
+document.querySelector(".past-news__button").onclick = () => {
+        for(let i = 0; i < blocks.children.length; i++){
+        let target = Math.floor(Math.random() * blocks.children.length -1) + 1;
+        blocks.children[i].style.order = `${target}`;
+    }
+};
 
