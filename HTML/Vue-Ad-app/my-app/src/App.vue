0012a481a0cbf945ad5ec6ffe5ff82500e451955
eduard-sd <pr-spektr@yandex.ru>
2020-01-04 20:13:59 +0300

    Reloading  Ad application on Vue Vuetify

---
+++ HTML/Vue-Ad-app/my-app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app temporary></v-navigation-drawer>
+
+    <v-app-bar app
+               color="primary"
+               dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Add application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text>Link 1</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+
+
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+      <!--<router-view></router-view>-->
+    </v-content>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+};
+</script>
