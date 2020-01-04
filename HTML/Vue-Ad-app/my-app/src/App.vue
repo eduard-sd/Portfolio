94ca3e52358d0f504423e06f58a759ff1fe207e2
eduard-sd <pr-spektr@yandex.ru>
2020-01-05 01:47:40 +0300

    Add sidebar, buttons, links, mobile adaptetion

--- HTML/Vue-Ad-app/my-app/src/App.vue
+++ HTML/Vue-Ad-app/my-app/src/App.vue
@@ -1,15 +1,46 @@
 <template>
   <v-app>
-    <v-navigation-drawer app temporary></v-navigation-drawer>
+    <v-navigation-drawer
+            app
+            temporary
+            v-model="drawer"
+    >
+      <v-list>
+        <v-list-item
+          v-for="link of links"
+          :key="link.title"
+          :to="link.url"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="link.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-app-bar app
                color="primary"
                dark>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+              @click="drawer = !drawer"
+              class="hidden-md-and-up"
+      ></v-app-bar-nav-icon>
       <v-toolbar-title>Add application</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn text>Link 1</v-btn>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn
+                text
+                v-for="link in links"
+                :key="link.title"
+                :to="link.url"
+        >
+          <v-icon left>{{ link.icon }}</v-icon>
+          {{ link.title }}
+        </v-btn>
       </v-toolbar-items>
     </v-app-bar>
 
@@ -17,7 +48,7 @@
 
     <!-- Sizes your content based upon application components -->
     <v-content>
-      <!--<router-view></router-view>-->
+      <router-view></router-view>
     </v-content>
 
   </v-app>
@@ -25,5 +56,18 @@
 
 <script>
 export default {
+  data () {
+    return {
+      drawer: false,
+      links: [
+        {title: 'Login', icon: 'mdi-lock', url: '/login'},
+        {title: 'Registration',icon: 'mdi-face', url: '/registration'},
+        {title: 'Orders', icon: 'mdi-bookmark', url: '/orders'},
+        {title: 'New ad', icon: 'mdi-note', url: '/new'},
+        {title: 'My ads', icon: 'mdi-pen ', url: '/list'},
+      ]
+    }
+  },
+
 };
 </script>
