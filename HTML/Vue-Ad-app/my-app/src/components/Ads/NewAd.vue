a4927f880d104af6b1f0fc06595568274c522b9c
eduard-sd <pr-spektr@yandex.ru>
2020-01-07 21:50:46 +0300

    Added new components Adlist, Newad, Orders. Modified package.json

--- HTML/Vue-Ad-app/my-app/src/components/Ads/NewAd.vue
+++ HTML/Vue-Ad-app/my-app/src/components/Ads/NewAd.vue
@@ -1,8 +1,67 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>NewAd</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text-secondary mb-3">Create new ad</h1>
+                <v-form ref="form" v-model="valid" lazy-validation class="mb-3">
+                    <v-text-field
+                            v-model="title"
+                            :rules="[v => !!v || 'Title is required']"
+                            label="Ad title"
+                            required
+                            prepend-icon="mdi-email"
+                            name="title"
+                            type="text"
+                    />
+
+                    <v-textarea
+                            v-model="description"
+                            label="Ad description"
+                            name="description"
+                            type="multi-line"
+                            required
+                            prepend-icon="mdi-lock"
+                            :rules="[v => !!v || 'Description is required']"
+                    />
+                </v-form>
+
+                <v-layout row>
+                    <v-flex xs12 class="mb-3">
+                        <v-btn
+                                color="blue-grey"
+                                class="ma-2 white--text"
+                        >
+                            Upload
+                            <v-icon right dark>mdi-cloud-upload</v-icon>
+                        </v-btn>
+                    </v-flex>
+                </v-layout>
+                <v-layout row>
+                    <v-flex xs12 class="mb-3">
+                        <img src="" alt="" height="100px">
+                    </v-flex>
+                </v-layout>
+                <v-layout row>
+                    <v-flex xs12 class="mb-3">
+                        <v-switch
+                                v-model="promo"
+                                label="Add to promo"
+                        ></v-switch>
+                    </v-flex>
+                </v-layout>
+                <v-layout row>
+                    <v-flex xs12>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                                :disabled="!valid"
+                                class="ma-2 success"
+                                @click="createAd"
+                        >
+                            Create ad
+                        </v-btn>
+                    </v-flex>
+                </v-layout>
+
             </v-flex>
         </v-layout>
     </v-container>
@@ -11,7 +70,25 @@
 <script>
     export default {
         data () {
-            return {}
+            return {
+                promo: false,
+                title: '',
+                description: '',
+                valid: false
+            }
+        },
+        methods: {
+            createAd () {
+                if (this.$refs.form.validate()) {
+                    //logic
+                    const ad = {
+                        title: this.title,
+                        description: this.description,
+                        promo: this.promo
+                    }
+                    console.log(ad)
+                }
+            }
         }
     }
 </script>
