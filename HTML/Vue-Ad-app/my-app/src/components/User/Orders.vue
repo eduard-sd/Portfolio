8c252a45c106886b561021f152d95261db4f1c51
sayahoved@rospartner.ru <sayahoved@rospartner.ru>
2020-01-07 16:03:22 +0300

    new component orders

--- HTML/Vue-Ad-app/my-app/src/components/User/Orders.vue
+++ HTML/Vue-Ad-app/my-app/src/components/User/Orders.vue
@@ -1,13 +1,62 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>Orders</h1>
+            <v-flex xs12 sm6 offset-sm-3>
+                <h1 class="text--secondary mb-3">Orders</h1>
+                <v-list
+                        subheader
+                        two-line
+                        flat
+                >
+                    <v-list-item
+                            v-for="order in orders"
+                            :key="order.id"
+                    >
+                        <v-list-item-action>
+                            <v-checkbox
+                                    @change="markDone(order)"
+                                    :input-value="order.done"
+                                    color="success"
+                            ></v-checkbox>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                            <v-list-item-title>{{ order.name }}</v-list-item-title>
+                            <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+                        </v-list-item-content>
+
+                        <v-list-item-action>
+                            <v-btn
+                                    :to="'/ad/'+order.adId"
+                                    class="primary"
+                            >Open</v-btn>
+                        </v-list-item-action>
+                    </v-list-item>
+                </v-list>
             </v-flex>
         </v-layout>
     </v-container>
 </template>
 
 <script>
-
+ export default {
+     data () {
+         return {
+             orders: [
+                 {
+                     id: '123',
+                     name: 'v',
+                     phone: '8',
+                     adId: '123',
+                     done: false
+                 }
+             ]
+         }
+     },
+     methods: {
+         markDone (order) {
+             order.done = true
+         }
+     }
+ }
 </script>
