e42422160971927140fdc9624b16dcb7768dc299
eduard-sd <pr-spektr@yandex.ru>
2020-01-12 21:41:22 +0300

    Adding firebase

--- HTML/Vue-Ad-app/my-app/src/components/Auth/Registration.vue
+++ HTML/Vue-Ad-app/my-app/src/components/Auth/Registration.vue
@@ -40,7 +40,12 @@
                     </v-card-text>
                     <v-card-actions>
                         <v-spacer />
-                        <v-btn color="primary" @click="onSubmit" :disabled="!valid">Create account!</v-btn>
+                        <v-btn
+                                color="primary"
+                                @click="onSubmit"
+                                :loading="loading"
+                                :disabled="!valid || loading"
+                        >Create account!</v-btn>
                     </v-card-actions>
                 </v-card>
             </v-col>
@@ -70,6 +75,11 @@
                 ],
             }
         },
+        computed: {
+            loading () {
+                return this.$store.getters.loading
+            }
+        },
         methods: {
             onSubmit () {
                 if (this.$refs.form.validate()) {
@@ -78,7 +88,13 @@
                         password: this.password
                     }
 
-                    alert(user)
+                    this.$store.dispatch('registerUser', user)
+                        .then(()=> {
+                            this.$router.push('/')
+                        })
+                        .catch(err => {
+                            console.log(err)
+                        })
                 }
             }
         }
--- HTML/Vue-Ad-app/my-app/src/main.js
+++ HTML/Vue-Ad-app/my-app/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import store from './store'
+import * as fb from 'firebase'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
@@ -10,5 +11,16 @@ new Vue({
   vuetify,
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    fb.initializeApp({
+      apiKey: "AIzaSyCKd-h7AjzhuUXSZzhw8WXW6NdG_MgKgt4",
+      authDomain: "eduard-sd-ads.firebaseapp.com",
+      databaseURL: "https://eduard-sd-ads.firebaseio.com",
+      projectId: "eduard-sd-ads",
+      storageBucket: "eduard-sd-ads.appspot.com",
+      messagingSenderId: "66001061538",
+      appId: "1:66001061538:web:cd281c75c48e697c9e79fb"
+    })
+  },
 }).$mount('#app')
