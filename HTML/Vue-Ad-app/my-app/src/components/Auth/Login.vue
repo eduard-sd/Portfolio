fc10918396da376b7b5b3e2cd1fe0fac66fae1f4
eduard-sd <pr-spektr@yandex.ru>
2020-01-22 21:45:24 +0300

    login

--- HTML/Vue-Ad-app/my-app/src/components/Auth/Login.vue
+++ HTML/Vue-Ad-app/my-app/src/components/Auth/Login.vue
@@ -31,7 +31,12 @@
                     </v-card-text>
                     <v-card-actions>
                         <v-spacer />
-                        <v-btn color="primary" @click="onSubmit" :disabled="!valid">Login</v-btn>
+                        <v-btn
+                                color="primary"
+                                @click="onSubmit"
+                                :loading="loading"
+                                :disabled="!valid || loading"
+                        >Login</v-btn>
                     </v-card-actions>
                 </v-card>
             </v-col>
@@ -56,6 +61,11 @@
                 ],
             }
         },
+        computed: {
+            loading () {
+               return this.$store.getters.loading
+            }
+        },
         methods: {
             onSubmit () {
                 if (this.$refs.form.validate()) {
@@ -64,7 +74,11 @@
                         password: this.password
                     }
 
-                    alert(user)
+                    this.$store.dispatch('loginUser', user)
+                        .then(() => {
+                            this.$router.push('/')
+                        })
+                        .catch(err => console.log(err))
                 }
             }
         }
